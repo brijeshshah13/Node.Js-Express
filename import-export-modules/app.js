d6179c51ae598e4e723942b94559cde4c65a749d
Brijesh Shah <brijeshjain13@gmail.com>
2017-11-25 19:40:04 +0530

    Update comments

--- import-export-modules/app.js
+++ import-export-modules/app.js
@@ -8,10 +8,10 @@ var app = express();
  * Home route (/)
  * This page can be accessed by going to http://example.com:3000/
  */
-app.get('/', (req, res) => res.send('The area of square with width 4 is' + square.area(4) 
+app.get('/', (req, res) => res.send('The area of square with width 4 is' + square.area(4)
   + '<br> The perimter of square with width 4 is ' + square.perimeter(4)));
 
 /**
- * Listen for connections in port 3000
+ * Listen for connections on port 3000
  */
 app.listen(3000, () => console.log('Started listening for connections on port 3000'));
